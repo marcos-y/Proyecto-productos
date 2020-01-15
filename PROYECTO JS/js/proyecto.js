a057646309299c5d37a8987a13de9c31518be990
myoldi <you@example.com>
2020-01-14 23:02:22 -0300

    cambioos

--- PROYECTO JS/js/proyecto.js
+++ PROYECTO JS/js/proyecto.js
@@ -1,5 +1,5 @@
 class Producto {
-    constructor(nombre, precio, categoria,descripcion) {
+    constructor(nombre, precio, categoria, descripcion) {
         this.nombre = nombre;
         this.precio = precio;
         this.categoria = categoria;
@@ -9,24 +9,60 @@ class Producto {
 
 
 var productos = [];
-var i = 0;
+
 
 var inputValue = () => {
     let value1 = document.getElementById("value1").value;
     let value2 = document.getElementById("value2").value;
     let value3 = document.getElementById("value3").value;
     let value4 = document.getElementById("value4").value;
-    i=i+1;
-    productos[i]= new Producto (value1,value2,value3,value4);
-    console.log(productos[i]);
+
+    let nuevoProd = new Producto(value1, value2, value3, value4);
+    productos.push(nuevoProd);
 
     //CREANDO EN EL DOM
-    const lista = document.getElementById("lista");
-    newProdu.innerHTML = `
-        <li class="list-group-item" id="li1">
-           test producto
-        </li>
-    `
-    
+    lista.innerHTML = lista.innerHTML + `
+        <li class="list-group-item" data-toggle="modal" data-target="#exampleModal">
+           ${value1}
+        </li>`;
 }
+
+
+var encontrar = () => {
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const arrayElementos = [];
+//const getValue = nombre => document.getElementByiD(NOMBRE).VALUE;
+//getValue('nombre');
+// onst renderListr = arrayMostrar
+//let listaDeeLEMMENTOS=0;
+
+//for (const nombre of arrayMostrar){
+//    listaDeElementos = listaDeElementos +  <li class="list-group-item" data-toggle="modal" data-target="#exampleModal">
+//    ${value1}
+// </li>`;
+// }
 
